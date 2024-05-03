4663e89543847b6f569c68aff3f26385dbd17438
Hector Ceballos Avilés <35620898+hceballos@users.noreply.github.com>
2024-05-03 13:11:19 -0400

    Add files via upload

---
+++ ejecutar_script.py
@@ -0,0 +1,11 @@
+# python mainValidacion.py --operation report
+# DisponibilidadDeDevengo - 121060101
+import sys
+sys.dont_write_bytecode = False  # Activa la generación de archivos .pyc
+
+
+
+import mi_script
+
+if __name__ == "__main__":
+    prueba = mi_script.Prueba()
