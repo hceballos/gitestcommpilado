b2089860544e30aac49785173088dde64e6f197b
Hector Ceballos Avilés <35620898+hceballos@users.noreply.github.com>
2024-08-04 02:27:07 -0400

    Update scrapingSigfeReports.py

--- scrapingSigfeReports.py
+++ scrapingSigfeReports.py
@@ -1,62 +1,27 @@
-import sqlalchemy
-import pandas as pd
-import glob
-from sqlalchemy import create_engine, Table, MetaData, Column, Integer, String
-import re
-import platform
-import time
-import sqlite3
-import re
-import platform
-import pandas as pd
-import glob
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException
-from selenium import webdriver
-from PyPDF2 import PdfReader
+import platform
 
-
-class scrapingSigfeReports():
-
-	def getMac(self):
-		chrome_options = webdriver.ChromeOptions()
-		prefs = {
-			'download.default_directory': '/Users/hector/Documents/Documents/desarrollo/convenios_y_transferencias/input_excel/resolucionesUrgencia/pdfs',
-			"download.prompt_for_download": False,
-			"download.directory_upgrade": True,
-			"safebrowsing_for_trusted_sources_enabled": False,
-			"safebrowsing.enabled": False
-		}
-		chrome_options.add_experimental_option('prefs', prefs)
-		chrome_options.add_argument('--ignore-certificate-errors')
-		chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-		chrome_options.binary_location = '..//convenios_y_transferencias//webdriver//chrome-mac//Chromium.app//Contents//MacOS//Chromium'  # Ruta a la versión de Chromium 114.0.5735.90
-		#chrome_options.add_argument('--headless')
-		driver = webdriver.Chrome(executable_path='..//convenios_y_transferencias//webdriver//chromedriver', chrome_options=chrome_options)
-		driver.maximize_window()
-		
-		driver.get('https://www.sis.mejorninez.cl/')
-		webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-		envioInformacion = Envio_Informacion()
-		envioInformacion.envio_Informacion_by_name(driver, "usuario", "hceballos@servicioproteccion.gob.cl")
-		envioInformacion.envio_Informacion_by_name(driver, "password", "Mejorninez1")
-		WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "ingresar"))).click()
-		driver.get("https://a1.sis.mejorninez.cl/mod_financiero/Pagos/wf_InformePagos.aspx")
-		time.sleep(5)
+class Main(object):
+	"""Clase principal para ejecutar el scraping"""
 
 	def __init__(self):
-		#self.datos = datos
+		chrome_options = webdriver.ChromeOptions()
 		sistema_operativo = platform.system()
 		if sistema_operativo == 'Darwin':
 			print("Estás utilizando un sistema Mac")
-			driver = self.getMac()
-			#driver.close()
-			#driver.quit()
+			chrome_options.add_argument('--ignore-certificate-errors')
+			chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+			chrome_options.binary_location = '/Users/hector/Documents/Documents/desarrollo/validadorUrgenciasCommpilado/webdriver/chrome-mac/Chromium.app/Contents/MacOS/Chromium'  # Ruta a la versión de Chromium
+			
+			# Crear el objeto Service para especificar el path del chromedriver
+			service = Service('/Users/hector/Documents/Documents/desarrollo/validadorUrgenciasCommpilado/webdriver/chrome-mac/Chromium.app/Contents/MacOS/Chromium')
+			
+			# Pasar el objeto service y las opciones al controlador de Chrome
+			driver = webdriver.Chrome(service=service)
+			driver.maximize_window()
 
 if __name__ == '__main__':
-	scrapingSigfeReports()
+	Main()
