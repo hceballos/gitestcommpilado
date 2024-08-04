14cbd45e114c259b551189b58a32d5bf40875f3e
Hector Ceballos Avilés <35620898+hceballos@users.noreply.github.com>
2024-08-04 00:54:47 -0400

    Update scrapingSigfeReports.py

--- scrapingSigfeReports.py
+++ scrapingSigfeReports.py
@@ -1,62 +1,142 @@
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
+import sqlalchemy
+import pandas as pd
+import glob
+from lib.fuente import Fuente
+from sqlalchemy import create_engine, Table, MetaData, Column, Integer, String
+import re
+import platform
+import time
+import sqlite3
+import re
+import platform
+import pandas as pd
+import glob
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-import re
-import sqlite3
-import pandas as pd
-import os
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium import webdriver
+from PyPDF2 import PdfReader
+from lib.fuente import Fuente
+from lib.elementos import Envio_Informacion, Click
+from lib.reliquidacion80Bis.scrapyProceso.tablaPagos import TablaPagos
 
-class scrapingSigfeReports:
-	
-	@staticmethod
-	def iniciar_driver():
-		"""
-		Inicializa el driver de Chrome con una ruta relativa para el chromedriver.
-		"""
-		# Definir la ruta relativa al chromedriver
-		ruta_relativa_chromedriver = os.path.join('chromedriver')
-		print(ruta_relativa_chromedriver)
-		# Obtener la ruta absoluta del chromedriver
-		# ruta_absoluta_chromedriver = os.path.abspath('..//repositorio_clonado//webdriver//chromedriver')
 
-		# Inicializar el driver de Chrome con la ruta absoluta del chromedriver
-		driver = webdriver.Chrome(executable_path='..//repositorio_clonado//webdriver//chromedriver')
+class main():
+
+	def getMac(self, query):
+		chrome_options = webdriver.ChromeOptions()
+		prefs = {
+			'download.default_directory': '/Users/hector/Documents/Documents/desarrollo/convenios_y_transferencias/input_excel/resolucionesUrgencia/pdfs',
+			"download.prompt_for_download": False,
+			"download.directory_upgrade": True,
+			"safebrowsing_for_trusted_sources_enabled": False,
+			"safebrowsing.enabled": False
+		}
+		chrome_options.add_experimental_option('prefs', prefs)
+		chrome_options.add_argument('--ignore-certificate-errors')
+		chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+		chrome_options.binary_location = '..//convenios_y_transferencias//webdriver//chrome-mac//Chromium.app//Contents//MacOS//Chromium'  # Ruta a la versión de Chromium 114.0.5735.90
+		#chrome_options.add_argument('--headless')
+		driver = webdriver.Chrome(executable_path='..//convenios_y_transferencias//webdriver//chromedriver', chrome_options=chrome_options)
+		driver.maximize_window()
 		
-		return driver
+		driver.get('https://www.sis.mejorninez.cl/')
+		webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+		envioInformacion = Envio_Informacion()
+		envioInformacion.envio_Informacion_by_name(driver, "usuario", "hceballos@servicioproteccion.gob.cl")
+		envioInformacion.envio_Informacion_by_name(driver, "password", "Mejorninez1")
+		WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "ingresar"))).click()
+		driver.get("https://a1.sis.mejorninez.cl/mod_financiero/Pagos/wf_InformePagos.aspx")
+		time.sleep(5)
 
-	def __init__(self):
-		options = Options()
-		options.headless = False
-		print("Inicializar el driver de Chrome")
-		# Inicializar el driver de Chrome
-		time.sleep(5000)
-		self.driver = self.iniciar_driver()
+		for index, row in query.iterrows():
+			# print(row)
+			WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "lnkLimpiar"))).click()  # BOTON BUSCAR
+			envioInforProyecto = Envio_Informacion()
+			time.sleep(2.5)
+			envioInforProyecto.envio_Informacion_by_name(driver, "txtPeriodo", row['MESANO'])
 
-		# Asignar las opciones al driver
-		self.driver.options = options
+			time.sleep(1.5)
+			try:
+				envioInforProyecto.envio_Informacion_by_name(driver, "I_ProyectoCodigo$txtCodigo", row['CODPROYECTO'])
+			except Exception as e:
+				print(f"Error: {e}")
+				envioInforProyecto.envio_Informacion_by_name(driver, "I_ProyectoCodigo$txtCodigo", row['CODPROYECTO'])
+			time.sleep(1.5)
+			WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "btnBuscarPagos"))).click()  # BOTON BUSCAR
+			time.sleep(2.5)
+		
+			TablaPagos(row, driver)
 
-		self.setUp(driver)
-
-	def setUp(self, driver):
-		fecha_desde = "2024-01-01"
-		fecha_hasta = "2024-12-31"
-		ejercicio   = "2024"
-		coberturas = ["2111001", "2111002", "2111003", "2111004", "2111005", "2111006", "2111007", "2111008", "2111009", "2111010", "2111011", "2111012", "2111013", "2111014", "2111015", "2111016", "2111017"]
 
 
-		i = 0
-		for cobertura in coberturas:
-			driver.execute_script("window.open('');")
-			ResultadoBusqueda = driver.switch_to.window(driver.window_handles[i])
-			urlDestino = "https://asin.sigfe.gob.cl/jasperserver-pro/flow.html?_flowId=viewReportFlow&reportUnit=/SIGFE2/Reportes/SB_DisponibilidadDevengoPresupuestario&pp=u=hceballos2111&fecha_desde="+fecha_desde+"&ejercicio="+ejercicio+"&codigo_moneda=CLP&TITLESUBTITULOREPORTE="+ cobertura +"%20Direcci%C3%B3n%20Nacional&TITLETIPOMONEDAREPORTE=Gasto%20-%20Nacional&TITLETITULOREPORTE=Disponibilidad%20de%20Devengos%20Presupuestarios&ambiente=SIGFE2&codigo_presupuesto=02&fecha_hasta="+fecha_hasta+"&unidad_ejecutora="+ cobertura +"&ambiente=SIGFE2&site=SB&standAlone=true&decorate=no&readOnly=true&userLocale=es"
-			driver.get(urlDestino)
-			i += 1
-		time.sleep(5000)
+	def getWin(self, query):
+		chrome_options = webdriver.ChromeOptions()
+		prefs = {
+			'download.default_directory': '/Users/hector/Documents/Documents/desarrollo/convenios_y_transferencias/input_excel/resolucionesUrgencia/pdfs',
+			"download.prompt_for_download": False,
+			"download.directory_upgrade": True,
+			"safebrowsing_for_trusted_sources_enabled": False,
+			"safebrowsing.enabled": False
+		}
+		chrome_options.add_experimental_option('prefs', prefs)
+		chrome_options.add_argument('--ignore-certificate-errors')
+		chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+		# chrome_options.binary_location = '..//convenios_y_transferencias//webdriver//chrome-mac//Chromium.app//Contents//MacOS//Chromium'  # Ruta a la versión de Chromium 114.0.5735.90
+		#chrome_options.add_argument('--headless')
+		driver = webdriver.Chrome(executable_path='..//convenios_y_transferencias//webdriver//chromedriver', chrome_options=chrome_options)
+		driver.maximize_window()
+		
+		driver.get('https://www.sis.mejorninez.cl/')
+		webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+		envioInformacion = Envio_Informacion()
+		envioInformacion.envio_Informacion_by_name(driver, "usuario", "hceballos@servicioproteccion.gob.cl")
+		envioInformacion.envio_Informacion_by_name(driver, "password", "Mejorninez1")
+		WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "ingresar"))).click()
+		driver.get("https://a1.sis.mejorninez.cl/mod_financiero/Pagos/wf_InformePagos.aspx")
+		time.sleep(5)
+
+		for index, row in query.iterrows():
+			WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "lnkLimpiar"))).click()  # BOTON BUSCAR
+			envioInforProyecto = Envio_Informacion()
+			time.sleep(2.5)
+			envioInforProyecto.envio_Informacion_by_name(driver, "txtPeriodo", row['MESANO'])
+
+			time.sleep(1.5)
+			try:
+				envioInforProyecto.envio_Informacion_by_name(driver, "I_ProyectoCodigo$txtCodigo", row['CODPROYECTO'])
+			except Exception as e:
+				print(f"Error: {e}")
+				envioInforProyecto.envio_Informacion_by_name(driver, "I_ProyectoCodigo$txtCodigo", row['CODPROYECTO'])
+			time.sleep(1.5)
+			WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "btnBuscarPagos"))).click()  # BOTON BUSCAR
+			time.sleep(2.5)
+		
+			TablaPagos(row, driver)
+
+
+	def __init__(self, query):
+		#self.datos = datos
+		sistema_operativo = platform.system()
+		if sistema_operativo == 'Darwin':
+			print("Estás utilizando un sistema Mac")
+			driver = self.getMac(query)
+			#driver.close()
+			#driver.quit()
+
+
+		elif sistema_operativo == 'Windows':
+			print("Estás utilizando un sistema Windows.")
+			driver = self.getWin(query)
+			driver.close()
+			driver.quit()
+
+
+
 
 if __name__ == '__main__':
-	scrapingSigfeReports()
+	main()
