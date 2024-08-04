1aa76be27cba9b90a69a67f0de7681470848e22d
Hector Ceballos Avilés <35620898+hceballos@users.noreply.github.com>
2024-08-04 02:50:01 -0400

    Update scrapingSigfeReports.py

--- scrapingSigfeReports.py
+++ scrapingSigfeReports.py
@@ -19,16 +19,16 @@ class Main(object):
 			# Crear el objeto Service para especificar el path del chromedriver
 			service = Service('/Users/hector/Documents/Documents/desarrollo/validadorUrgenciasCommpilado/webdriver/chrome-mac/Chromium.app/Contents/MacOS/Chromium')
 			
-            # Pasar el objeto service y las opciones al controlador de Chrome
-            try:
-                driver = webdriver.Chrome(service=service, options=chrome_options)
-                driver.maximize_window()
-                
-                # Intentar abrir la página web especificada
-                driver.get("https://www.sis.mejorninez.cl/")
-                print("Página abierta con éxito")
-            except Exception as e:
-                print(f"Error al abrir la página: {e}")
+			# Pasar el objeto service y las opciones al controlador de Chrome
+			try:
+				driver = webdriver.Chrome(service=service, options=chrome_options)
+				driver.maximize_window()
+				
+				# Intentar abrir la página web especificada
+				driver.get("https://www.sis.mejorninez.cl/")
+				print("Página abierta con éxito")
+			except Exception as e:
+				print(f"Error al abrir la página: {e}")
 
 if __name__ == '__main__':
 	Main()
